067465200895f383f24b816f6fe1cde136759b92
Abash Ansari <Ansariabash2004@gmail.com>
2025-01-23 05:03:53 -0800

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main (){
+     int a;
+     printf("Enter a Number :");
+     scanf("%d",&a);
+     switch(a){
+        case 1:
+              printf(" your morning ");
+              break;
+        case 2 :
+               printf(" your afternoon ");
+               break;
+        case 3:
+              printf(" your dinner");
+              break;
+        default:
+               printf("Kuch V");
+              }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
